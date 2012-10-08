f9479a666e3795ca06fd094a3c6a04b87906dfa8
Zack Liscio <zackliscio@gmail.com>
2012-10-08 14:14:19 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offerspostdeck::Application.config.secret_token = 'e6704c8b4f51f2d11ff5ffd58bdb46c7d8dda3aab6dfb32efb2bcd40aec175898ab460142b087afe3c00d3391601d6ab79164d8e83e796aec85d24a2db436985'
