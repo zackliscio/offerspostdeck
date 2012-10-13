44ae58f824e4b1c2bb9dc336c8ad88ac819e9b06
Zack Liscio <zackliscio@gmail.com>
2012-10-12 21:20:57 -0700

    added line to config init

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,6 @@
 # config/initializers/pdfkit.rb
 PDFKit.configure do |config|
+  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env.production? 
   # config.wkhtmltopdf = '/path/to/wkhtmltopdf'
   config.default_options = {
     :orientation => 'Landscape',
